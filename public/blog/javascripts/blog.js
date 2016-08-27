bae71da7f45a350573aff3d82f9550a63ece1af9
Jollen Chen <jollen@moko365.com>
2016-08-27 12:10:15 +0800

    bug fix: push server

--- public/blog/javascripts/blog.js
+++ public/blog/javascripts/blog.js
@@ -1,7 +1,9 @@
+"use strict";
+
 (function() {
 	var el = $('[data-posts]');
 
-	var ws = new WebSocket("ws://localhost:8080/", ['echo-protocol']);
+	var ws = new WebSocket("ws://push.flowchainjs.com/", ['echo-protocol']);
 
 	ws.onopen = function() {
 		console.log('websocket server connected');
@@ -10,5 +12,5 @@
 	ws.onmessage = function(message) {
 		var payload = JSON.parse(message.data);
 		el.html(payload.data);
-	}
+	};
 }());
